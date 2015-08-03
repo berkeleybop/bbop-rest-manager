66c7641e5e8dc6566619c92375126ae01a097d0c
SC <github.com@genkisugi.net>
2015-08-02 22:27:02 -0700

    doc bump

--- doc/scripts/jsdoc-toc.js
+++ doc/scripts/jsdoc-toc.js
@@ -6,7 +6,7 @@
     treeNode.tree({
         autoEscape: false,
         closedIcon: '&#x21e2;',
-        data: [{"label":"<a href=\"module-bbop-rest-manager.html\">bbop-rest-manager</a>","id":"module:bbop-rest-manager","children":[{"label":"<a href=\"module-bbop-rest-manager-manager.html\">manager</a>","id":"module:bbop-rest-manager~manager","children":[]}]}],
+        data: [{"label":"<a href=\"module-bbop-rest-manager.html\">bbop-rest-manager</a>","id":"module:bbop-rest-manager","children":[{"label":"<a href=\"module-bbop-rest-manager-manager_base.html\">manager_base</a>","id":"module:bbop-rest-manager~manager_base","children":[]},{"label":"<a href=\"module-bbop-rest-manager-manager_jquery.html\">manager_jquery</a>","id":"module:bbop-rest-manager~manager_jquery","children":[]},{"label":"<a href=\"module-bbop-rest-manager-manager_node.html\">manager_node</a>","id":"module:bbop-rest-manager~manager_node","children":[]},{"label":"<a href=\"module-bbop-rest-manager-manager_node_sync.html\">manager_node_sync</a>","id":"module:bbop-rest-manager~manager_node_sync","children":[]}]}],
         openedIcon: ' &#x21e3;',
         saveState: true,
         useContextMenu: false
