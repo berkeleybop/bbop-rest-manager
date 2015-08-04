3693e8f60ebc399e5f9d5a17043c5ba7f818e61b
SC <github.com@genkisugi.net>
2015-08-04 12:51:00 -0700

    fixes to get tests working again; tests for all payload combinations

--- tests/core.test.js
+++ tests/core.test.js
@@ -7,20 +7,40 @@ chai.config.includeStack = true;
 var assert = chai.assert;
 var managers = require('..');
 
+var us = require('underscore');
+var each = us.each;
+
 var manager_base = managers.base;
 var manager_node = managers.node;
 var manager_sync_request = managers.sync_request;
 var manager_jquery = managers.jquery;
+
+// var manager_meta_list = [
+//     {'name': 'node',
+//      'object': manager_node,
+//      'type': 'async'}, 
+//     {'name': 'sync_request',
+//      'object': manager_sync_request,
+//      'type': 'sync'}, 
+//     {'name': 'jquery',
+//      'object': manager_jquery,
+//      'type': 'async'}
+// ];
 
 // Correct environment, ready testing.
 var bbop = require('bbop-core');
 var response_base = require('bbop-rest-response').base;
 var response_json = require('bbop-rest-response').json;
 
+// Standard test parameters.
+var start_port = 3344;
+var target = 'http://localhost:' + start_port.toString();
+
 ///
 /// Start unit testing.
 ///
 
+// No test server.
 describe('bbop-rest-manager#base + bbop-rest-response#base', function(){
 
     it('basic sync (watch callback)', function(){
@@ -111,124 +131,194 @@ describe('bbop-rest-manager#base + bbop-rest-response#json', function(){
 
 describe('bbop-rest-manager#node + bbop-rest-response#json', function(){
     
-    it('basic successful async (callbacks)', function(){
+    it('basic successful async (callbacks)', function(done){
 	
-	var target = 'http://amigo.geneontology.org/amigo/term/GO:0022008/json';
+    	var path = '/';
      
-	var m = new manager_node(response_json);
-	m.register('success', function(resp, man){
-	    var type = resp.raw()['type'];
-	    assert.equal(type, 'term', 'success callback');
-	});
-	m.register('error', function(resp, man){
-	    assert.equal(true, false, 'error callback is not expected');
-	});	    
-	m.start(target);
+    	var m = new manager_node(response_json);
+    	m.register('success', function(resp, man){
+    	    var text = resp.raw()['text'];
+    	    assert.equal(text, 'hello world', 'success callback');
+	    done();
+    	});
+    	m.register('error', function(resp, man){
+    	    assert.equal(true, false, 'error callback is not expected');
+	    done();
+    	});	    
+    	m.start(target + path);
     });
 
-    it('basic successful async (promise)', function(){
+    it('basic successful async (promise)', function(done){
 	
-	var target = 'http://amigo.geneontology.org/amigo/term/GO:0022008/json';
+    	var path = '/';
 	
-	var m = new manager_node(response_json);
-	var d = m.start(target);
-	d.then(function(resp){
-	    //console.log('resp', resp);
-	    var type = resp.raw()['type'];
-	    assert.equal(type, 'term', 'success callback');
-	}).done();
+    	var m = new manager_node(response_json);
+    	var d = m.start(target + path);
+    	d.then(function(resp){
+    	    var text = resp.raw()['text'];
+    	    assert.equal(text, 'hello world', 'success callback');
+	    done();
+    	}).done();
 	
     });
 
-    it('basic error async (callback)', function(){
+    it('basic error async (callback)', function(done){
 	
-	// Remote 500 error.
-	var target = 'http://amigo.geneontology.org/amigo/term/GO:0022008/jso';
+    	// Remote 500 error.
+    	var path = '/error';
      
-	var m = new manager_node(response_json);
-	m.register('error', function(resp, man){
-	    assert.equal(true, true, 'successful failure');
-	});	    
-	m.start(target);
+    	var m = new manager_node(response_json);
+    	m.register('error', function(resp, man){
+    	    assert.equal(true, true, 'successful failure');
+	    done();
+    	});	    
+    	m.start(target + path);
+
+    });
+
+    it('basic error async (promise)', function(done){
+	
+    	var path = '/error';
+	
+    	var m = new manager_node(response_json);
+    	var d = m.start(target + path);
+    	d.then(function(resp){
+	    //console.log(resp);
+    	    assert.equal(resp.okay(), false, 'bad response promise');
+	    done();
+    	}).done();
 	
     });
 
-    it('basic error async (promise)', function(){
+    it('see if we can actually supply payload arguments (GET)', function(done){
+
+    	// We want q=foo
+    	var path = '/';
+    	var pay = {'q': 'foo'};
+    	var meth = 'GET';
 	
-	var target = 'http://amigo.geneontology.org/amigo/term/GO:0022008/jso';
+    	var m = new manager_node(response_json);
+    	m.start(target + path, pay, meth).then(function(resp){
+    	    //console.log('resp',resp);
+    	    var q = resp.raw()['q'];
+    	    assert.equal(q, 'foo', 'payload success for ' + meth);
+    	    done();
+    	}).done();
+    });
+
+    it('see if we can actually supply payload arguments (POST)', function(done){
+
+    	// We want q=foo
+    	var path = '/';
+    	var pay = {'q': 'foo'};
+    	var meth = 'POST';
 	
-	var m = new manager_node(response_json);
-	var d = m.start(target);
-	d.then(function(resp){
-	    //console.log('resp', resp);
-	    //var type = resp.raw()['type'];
-	    assert.equal(resp.okay(), false, 'bad response promise');
-	}).done();
-	
+    	var m = new manager_node(response_json);
+    	m.start(target + path, pay, meth).then(function(resp){
+    	    //console.log('resp',resp);
+    	    var q = resp.raw()['q'];
+    	    assert.equal(q, 'foo', 'payload success for ' + meth);
+    	    done();
+    	}).done();
     });
 
 });
 
 describe('bbop-rest-manager#sync_request + bbop-rest-response#json', function(){
     
-    it('basic successful sync (direct reponse)', function(){
+    it('basic successful sync (direct reponse)', function(done){
 	
-    	var target = 'http://amigo.geneontology.org/amigo/term/GO:0022008/json';
+    	var path = '/';
      
     	var m = new manager_sync_request(response_json);
-    	var resp = m.fetch(target);
-    	var type = resp.raw()['type'];
-    	assert.equal(type, 'term', 'success callback');
-	
+    	var resp = m.fetch(target + path);
+    	var text = resp.raw()['text'];
+    	assert.equal(text, 'hello world', 'success callback');
+	done();
     });
 
-    it('basic successful sync (callback)', function(){
+    it('basic successful sync (callback)', function(done){
 	
-    	var target = 'http://amigo.geneontology.org/amigo/term/GO:0022008/json';
+    	var path = '/';
      
     	var m = new manager_sync_request(response_json);
     	m.register('success', function(resp, man){
-    	    var type = resp.raw()['type'];
-    	    assert.equal(type, 'term', 'success callback');
+    	    var text = resp.raw()['text'];
+    	    assert.equal(text, 'hello world', 'success callback');
+	    done();
     	});
     	m.register('error', function(resp, man){
     	    assert.equal(true, false, 'error callback is not expected');
+	    done();
     	});	    
-    	var qurl = m.fetch(target);
+    	var qurl = m.fetch(target + path);
 	
     });
 
-    it('basic error sync (callback)', function(){
+    it('basic error sync (callback)', function(done){
 	
     	// Remote 500 error.
-    	var target = 'http://amigo.geneontology.org/amigo/term/GO:0022008/jso';
+	var path = '/error';
      
     	var m = new manager_sync_request(response_json);
     	m.register('success', function(resp, man){
-    	    var type = resp.raw()['type'];
-    	    assert.equal(true, false, 'success callback is not expected');
+    	    assert.equal(true, false, 'syncr success callback is not expected');
+	    done();
     	});
     	m.register('error', function(resp, man){
     	    assert.equal(true, true, 'successful failure');
+	    done();
     	});	    
-    	m.fetch(target);
+    	m.fetch(target + path);
     });
 
-    it('basic success sync (instant deferred)', function(){
+    it('basic success sync (instant deferred)', function(done){
 	
-    	var target = 'http://amigo.geneontology.org/amigo/term/GO:0022008/json';
+	var path = '/';
      
     	var m = new manager_sync_request(response_json);
-    	m.start(target).then(function(resp){
-    	    var type = resp.raw()['type'];
-    	    assert.equal(true, false, 'success callback is not expected');
+    	m.start(target + path).then(function(resp){
+    	    var text = resp.raw()['text'];
+    	    assert.equal(text, 'hello world', 'success callback not expected');
+	    done();
 	}).done();
+    });
+
+    it('see if we can actually supply payload arguments (GET)', function(done){
+
+    	// We want q=foo
+	var path = '/';
+	var pay = {'q': 'foo'};
+	var meth = 'GET';
+	
+    	var m = new manager_sync_request(response_json);
+    	m.start(target + path, pay, meth).then(function(resp){
+	    //console.log('resp',resp);
+    	    var q = resp.raw()['q'];
+    	    assert.equal(q, 'foo', 'payload success for ' + meth);
+	    done();
+    	}).done();
+    });
+
+    it('see if we can actually supply payload arguments (POST)', function(done){
+
+    	// We want q=foo
+	var path = '/';
+	var pay = {'q': 'foo'};
+	var meth = 'POST';
+	
+    	var m = new manager_sync_request(response_json);
+    	m.start(target + path, pay, meth).then(function(resp){
+	    //console.log('resp',resp);
+    	    var q = resp.raw()['q'];
+    	    assert.equal(q, 'foo', 'payload success for ' + meth);
+	    done();
+    	}).done();
     });
 
 });
 
 describe('bbop-rest-manager#jquery + bbop-rest-response#json', function(){
-    
 
     var mock_jQuery = null;
     before(function(){
@@ -243,40 +333,78 @@ describe('bbop-rest-manager#jquery + bbop-rest-response#json', function(){
 	};
     });
 
-    it('test out mock jquery call with callback', function(){
+    it('test out mock jquery call with callback', function(done){
 	
-    	var target = 'http://amigo.geneontology.org/amigo/term/GO:0022008/json';
+	var path = '/';
      
 	// Goose jQuery into functioning here.
     	var m = new manager_jquery(response_json);
 	m.JQ = mock_jQuery;
 
     	m.register('success', function(resp, man){
-    	    var type = resp.raw()['type'];
-    	    assert.equal(true, true, 'successful failure');
+    	    var text = resp.raw()['text'];
+    	    assert.equal(text, 'hello world', 'successful failure');
+	    done();
     	});
     	m.register('error', function(resp, man){
-    	    assert.equal(true, false, 'success callback is not expected');
+    	    assert.equal(true, false, 'jquery success callback is not expected');
+	    done();
     	});	    
-    	m.start(target);
+    	m.start(target + path);
 
     });
 
-    it('test out mock jquery call with promise', function(){
+    it('test out mock jquery call with promise', function(done){
 	
-    	var target = 'http://amigo.geneontology.org/amigo/term/GO:0022008/json';
+	var path = '/';
      
 	// Goose jQuery into functioning here.
     	var m = new manager_jquery(response_json);
 	m.JQ = mock_jQuery;
 
 	// Try it out.
-    	m.start(target).then(function(resp){
-	    //console.log('resp', resp);
-    	    var type = resp.raw()['type'];
-    	    assert.equal(type, 'term', 'success callback is not expected');
+    	m.start(target + path).then(function(resp){
+    	    var text = resp.raw()['text'];
+    	    assert.equal(text, 'hello world', 'success callback not expected');
+	    done();
 	}).done();
 
+    });
+
+    it('see if we can actually supply payload arguments (GET)', function(done){
+
+    	// We want q=foo
+    	var path = '/';
+    	var pay = {'q': 'foo'};
+    	var meth = 'GET';
+	
+    	var m = new manager_jquery(response_json);
+	m.JQ = mock_jQuery;
+
+    	m.start(target + path, pay, meth).then(function(resp){
+    	    //console.log('resp',resp);
+    	    var q = resp.raw()['q'];
+    	    assert.equal(q, 'foo', 'payload success for ' + meth);
+    	    done();
+    	}).done();
+    });
+
+    it('see if we can actually supply payload arguments (POST)', function(done){
+
+    	// We want q=foo
+    	var path = '/';
+    	var pay = {'q': 'foo'};
+    	var meth = 'POST';
+	
+    	var m = new manager_jquery(response_json);
+	m.JQ = mock_jQuery;
+
+    	m.start(target + path, pay, meth).then(function(resp){
+    	    //console.log('resp',resp);
+    	    var q = resp.raw()['q'];
+    	    assert.equal(q, 'foo', 'payload success for ' + meth);
+    	    done();
+    	}).done();
     });
 
 });
